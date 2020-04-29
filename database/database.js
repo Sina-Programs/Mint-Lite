c437a04971322d6b537526f9ae83e8a69c1d9a70
Austin Douse <douse.austin@gmail.com>
2020-04-29 10:43:25 -0400

    server set up

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const sequelize = require("sequelize");
 
 // create "budget"
 
-const db = new sequelize("budgets", "root", "mySQLDBMS", {
+const db = new sequelize("budgets", "root", "Astnds19!", {
   dialect: "mysql",
 });
 
@@ -15,9 +15,9 @@ const Transactions = db.define("transactions", {
 });
 
 const Accounts = db.define("accounts", {
-    account_name: { type: sequelize.STRING },
-    transaction_type: { type: sequelize.STRING },
-})
+  account_name: { type: sequelize.STRING },
+  transaction_type: { type: sequelize.STRING },
+});
 
 Transactions.sync();
 Accounts.sync();
