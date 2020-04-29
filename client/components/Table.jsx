254ed5853cc78b5359e37db95e16cfeee1b0b383
Sina Nikmaram <NikmaramCareer@gmail.com>
2020-04-29 19:53:51 -0400

    center table in display

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -2,25 +2,27 @@ import React from "react";
 
 function Table({ transactions }) {
   return (
-    <div className="txn">
-      <table className="table is-striped is-fullwidth">
-        <thead className="tr">
-          <th className="td">Date</th>
-          <th className="td">Description</th>
-          <th className="td">Amount</th>
-          <th className="td">Type</th>
-        </thead>
-        {transactions.map((transaction) => {
-          return (
-            <tr className="tr" key={transaction.id}>
-              <td className="td">{transaction.txn_date}</td>
-              <td className="td">{transaction.txn_description}</td>
-              <td className="td">{transaction.txn_amount}</td>
-              <td className="td">{transaction.txn_type}</td>
-            </tr>
-          );
-        })}
-      </table>
+    <div className="txn columns is-centered">
+      <div className="column is-three-quarters">
+        <table className="table is-striped is-fullwidth">
+          <thead className="tr">
+            <th className="td">Date</th>
+            <th className="td">Description</th>
+            <th className="td">Amount</th>
+            <th className="td">Type</th>
+          </thead>
+          {transactions.map((transaction) => {
+            return (
+              <tr className="tr" key={transaction.id}>
+                <td className="td">{transaction.txn_date}</td>
+                <td className="td">{transaction.txn_description}</td>
+                <td className="td">{transaction.txn_amount}</td>
+                <td className="td">{transaction.txn_type}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </div>
   );
 }
