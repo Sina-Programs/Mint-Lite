f92b1d72541f21c0c161cdebc386242e198b7dd5
Sina Nikmaram <NikmaramCareer@gmail.com>
2020-04-30 11:47:29 -0400

    implement table and display styling

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -2,26 +2,50 @@ import React from "react";
 
 function Table({ transactions }) {
   return (
-    <div className="txn columns is-centered">
-      <div className="column is-three-quarters">
-        <table className="table is-striped is-fullwidth">
-          <thead className="tr">
-            <th className="td">Date</th>
-            <th className="td">Description</th>
-            <th className="td">Amount</th>
-            <th className="td">Type</th>
-          </thead>
-          {transactions.map((transaction) => {
-            return (
-              <tr className="tr" key={transaction.id}>
-                <td className="td">{transaction.txn_date}</td>
-                <td className="td">{transaction.txn_description}</td>
-                <td className="td">{transaction.txn_amount}</td>
-                <td className="td">{transaction.txn_type}</td>
-              </tr>
-            );
-          })}
-        </table>
+    <div>
+      <div className="columns is-centered">
+        <div className="column is-three-quarters">
+          <table className="table is-striped is-bordered is-fullwidth">
+            <tr>
+              <th className="td" style={{ width: "25%" }}>
+                Date
+              </th>
+              <th className="td" style={{ width: "25%" }}>
+                Description
+              </th>
+              <th className="td" style={{ width: "25%" }}>
+                Amount
+              </th>
+              <th className="td" style={{ width: "25%" }}>
+                Type
+              </th>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div className="columns is-centered">
+        <div className="column is-three-quarters" id="tableDiv">
+          <table className="table is-striped is-fullwidth">
+            {transactions.map((transaction) => {
+              return (
+                <tr className="tr" key={transaction.id}>
+                  <td className="td" style={{ width: "25%" }}>
+                    {transaction.txn_date}
+                  </td>
+                  <td className="td" style={{ width: "25%" }}>
+                    {transaction.txn_description}
+                  </td>
+                  <td className="td" style={{ width: "25%" }}>
+                    {transaction.txn_amount}
+                  </td>
+                  <td className="td" style={{ width: "25%" }}>
+                    {transaction.txn_type}
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
       </div>
     </div>
   );
